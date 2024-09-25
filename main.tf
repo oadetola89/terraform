4a8b0b42b53162dc6354093b965b798b5c468c63
root <root@test02.adetola.com>
2024-09-25 09:23:42 +0000

    First commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "test_instance" {
+  ami           = "ami-0cb117bb45fffa44c"
+  instance_type = "t3.medium"
+  iam_instance_profile = "AWSSupportPatchwork-SSMRoleForInstances"
+
+  tags = {
+    Name = "testserver001"
+  }
+}
