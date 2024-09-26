42708382fe672399aaf25993cf0f2d7c0ca5fd1f
Oluwatobi Adetola <adetoolu@amazon.com>
2024-09-26 08:50:56 +0000

    Add output variable

--- main.tf
+++ main.tf
@@ -8,6 +8,18 @@ variable "http_port" {
   default     = 80
 }
 
+output "securitygroup_id" {
+  value       = aws_security_group.instance.id
+  description = "The id of the security group"
+  sensitive   = false
+}
+
+output "instancepublic_ip" {
+  value       = aws_instance.test_instance.public_ip
+  description = "Public IP address of the instance"
+  sensitive   = false
+}
+
 resource "aws_instance" "test_instance" {
   ami           = "ami-0cb117bb45fffa44c"
   instance_type = "t3.medium"
