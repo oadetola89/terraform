d90c3e68c9944c42ac54c6a69b22dd51d3b3ff4a
Oluwatobi Adetola <adetoolu@amazon.com>
2024-09-26 06:09:17 +0000

    added security group

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "test_instance" {
   ami           = "ami-0cb117bb45fffa44c"
   instance_type = "t3.medium"
   iam_instance_profile = "AWSSupportPatchwork-SSMRoleForInstances"
+  security_groups =  "sg-0dc2a00131315cedc"
 
   tags = {
     Name = "testserver001"
