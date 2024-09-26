025e0d68d471d368c05bbd7aa816c8a15ba37664
Oluwatobi Adetola <adetoolu@amazon.com>
2024-09-26 07:15:01 +0000

    add userdata script

--- main.tf
+++ main.tf
@@ -6,6 +6,8 @@ resource "aws_instance" "test_instance" {
   ami           = "ami-0cb117bb45fffa44c"
   instance_type = "t3.medium"
   iam_instance_profile = "AWSSupportPatchwork-SSMRoleForInstances"
+  
+  user_data = "userdata"
 
   tags = {
     Name = "testserver001"
