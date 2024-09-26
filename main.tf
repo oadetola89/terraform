c323c76e09854a2a70356936f151526d60ab5ee5
Oluwatobi Adetola <adetoolu@amazon.com>
2024-09-26 07:40:19 +0000

    Add security group

--- main.tf
+++ main.tf
@@ -6,10 +6,21 @@ resource "aws_instance" "test_instance" {
   ami           = "ami-0cb117bb45fffa44c"
   instance_type = "t3.medium"
   iam_instance_profile = "AWSSupportPatchwork-SSMRoleForInstances"
-  
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
   user_data = file("/terraform/userdata")
 
   tags = {
     Name = "testserver001"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "testinstanceSG"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
 }
