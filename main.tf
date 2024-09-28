b4162f33ed309463b8d3b9daefc43524623609d9
Oluwatobi Adetola <adetoolu@amazon.com>
2024-09-28 02:42:28 +0000

    First Commit

--- main.tf
+++ main.tf
@@ -10,10 +10,30 @@ variable "http_port" {
   default     = 80
 }
 
+variable "vpc_id" {
+  default     = "vpc-0dcc13858f0dc8a74"
+}
+
 output "securitygroup_id" {
-  value       = aws_security_group.instance.id
+  value       = aws_security_group.test.id
   description = "The id of the security group"
   sensitive   = false
+}
+
+data "aws_vpc" "default" {
+  id  = "vpc-0dcc13858f0dc8a74"
+}
+
+data "aws_subnet" "default" {
+  filter {
+    name  = "vpc-id"
+    values  = [var.vpc_id]
+  }
+
+  filter {
+    name = "tag:Name"
+    values  = ["Production-US-East-1-subnet-public1-us-east-1b"]
+  }
 }
 
 output "instancepublic_ip" {
@@ -22,22 +42,10 @@ output "instancepublic_ip" {
   sensitive   = false
 }
 
-resource "aws_instance" "test_instance" {
-  ami           = "ami-0cb117bb45fffa44c"
-  instance_type = "t3.medium"
-  iam_instance_profile = "AWSSupportPatchwork-SSMRoleForInstances"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+resource "aws_security_group" "test" {
+  name = "testinstance-test0001-SG"
+  vpc_id  = var.vpc_id
 
-  user_data = file("/terraform/userdata")
-
-  tags = {
-    Name = "testserver001"
-    "awssupport:patchwork" = "patch"
-  }
-}
-
-resource "aws_security_group" "instance" {
-  name = "testinstanceSG"
   ingress {
     from_port = var.http_port
     to_port = var.http_port
@@ -46,8 +54,46 @@ resource "aws_security_group" "instance" {
   }
   egress {
     from_port = 0
-    to_port   = 0
-    protocol  = -1
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_network_interface" "primary" {
+  subnet_id   = data.aws_subnet.default.id
+  security_groups  = [aws_security_group.test.id]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "test_instance" {
+  ami           = "ami-0e54eba7c51c234f6"
+  instance_type = "t3.medium"
+  iam_instance_profile = "AWSSupportPatchwork-SSMRoleForInstances"
+  key_name = "myrsakey"
+
+  user_data = file("/terraform/userdata")
+
+  tags = {
+    Name = "testserver001"
+    "awssupport:patchwork" = "patch"
+  }
+
+  network_interface {
+    network_interface_id = aws_network_interface.primary.id
+    device_index         = 0
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "adetoolu-terraform-files"
+    key  = "workspace_instance/terraform.tfstate"
+    region  = "us-east-1"
+    dynamodb_table  = "terraform_state-locks"
+    encrypt  = true
   }
 }
