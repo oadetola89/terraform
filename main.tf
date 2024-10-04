c09a4c8ce6c14355602b41e8bc0461db0cd1ae0b
Oluwatobi Adetola <adetoolu@amazon.com>
2024-10-04 10:51:00 +0000

    Changing codes to module

--- main.tf
+++ main.tf
@@ -4,76 +4,14 @@ provider "aws" {
   profile                  = "default"
 }
 
-data "aws_vpc" "default" {
-  id  = "vpc-0dcc13858f0dc8a74"
-}
-
-data "aws_subnet" "default" {
-  filter {
-    name  = "vpc-id"
-    values  = [var.vpc_id]
-  }
-
-  filter {
-    name = "tag:Name"
-    values  = ["Production-US-East-1-subnet-public1-us-east-1b"]
-  }
-}
-
-data "terraform_remote_state" "db" {
-  backend = "s3"
-
-  config = {
-    bucket = "(adetoolu-terraform-files)"
-    key  = "database/mysql/terraform.tfstate"
-    region  = "us-east-1"
-  }
-}
-
-resource "aws_security_group" "test" {
-  name = "testinstance-test0001-SG"
-  vpc_id  = var.vpc_id
-
-  ingress {
-    from_port = var.http_port
-    to_port = var.http_port
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_network_interface" "primary" {
-  subnet_id   = data.aws_subnet.default.id
-  security_groups  = [aws_security_group.test.id]
-
-  tags = {
-    Name = "primary_network_interface"
-  }
-}
-
-resource "aws_instance" "test_instance" {
-  ami           = "ami-0e54eba7c51c234f6"
-  instance_type = "t3.medium"
-  iam_instance_profile = "AWSSupportPatchwork-SSMRoleForInstances"
-  key_name = "myrsakey"
-
-  user_data = file("/terraform/userdata")
-
-  tags = {
-    Name = "testserver001"
-    "awssupport:patchwork" = "patch"
-  }
-
-  network_interface {
-    network_interface_id = aws_network_interface.primary.id
-    device_index         = 0
-  }
+module "webserver" {
+  source = "/modules/apps/webserver"
+  instance_type  = "t3.medium"
+  vpc_id  = "vpc-0dcc13858f0dc8a74"
+  ami_id  = "ami-0e54eba7c51c234f6"
+  security_group_name  = "testserver001SG"
+  server_name  = "testserver001"
+  subnet_name  = "Production-US-East-1-subnet-public1-us-east-1b"
 }
 
 terraform {
